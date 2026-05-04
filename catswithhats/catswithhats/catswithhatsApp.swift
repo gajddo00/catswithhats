7eb4cf8bceb91f2bd4909836766b9adbf23c9ba1
Dominika Gajdova <dominika.gajdova@gnj.cz>
2026-05-04 12:15:22 +0200

    chore: initial project

---
+++ catswithhats/catswithhats/catswithhatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  catswithhatsApp.swift
+//  catswithhats
+//
+//  Created by Dominika Gajdová on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct catswithhatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
